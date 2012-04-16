f1d1fd9c391ac0fcc5471d5a36a3c3dfb4aa3cc0
Brian Victor <github@brianhv.org>
2012-04-16 15:31:39 -0400

    Separated responding to vim into one function to choose the response and another to encode it.

--- Main.hs
+++ Main.hs
@@ -1,23 +1,31 @@
-import Control.Monad (void)
 import Control.Monad.IO.Class
-import Data.Monoid (mempty)
 import Data.Conduit.Binary as CB
-import Data.Conduit.List as CL
+import qualified Data.Conduit.List as CL
 import Data.Conduit.Network
 import Data.Conduit
-import Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Char8 as BS
 import Data
 import Parser
 import Serializer (encodeCommand)
 
 socketHandler :: (MonadIO m) => Application m
-socketHandler src snk = src $= CB.lines $= lineToEvent $= logEvent $= CL.map pickResponse $$ snk -- $= lineToEvent $$ undefined
+socketHandler src snk = src
+                     $= CB.lines
+                     $= lineToEvent
+                     $= logEvent
+                     $= CL.map pickResponses
+                     $= CL.map encodeResponses
+                     $$ snk
 
-pickResponse :: VimEventType -> ByteString
-pickResponse StartupDone = encodeCommand (BufferID 1) (SequenceNum 1) $ EditFile "/tmp/test.txt"
-pickResponse _ = BS.pack ""
+pickResponses :: VimEventType -> [VimCommandType]
+pickResponses StartupDone = [EditFile "/tmp/test.txt"]
+pickResponses _ = []
 
-lineToEvent :: (Monad i) => Conduit ByteString i VimEventType
+encodeResponses :: [VimCommandType] -> BS.ByteString
+encodeResponses = BS.concat . map mkCommand where
+    mkCommand = encodeCommand (BufferID 1) (SequenceNum 1)
+
+lineToEvent :: (Monad i) => Conduit BS.ByteString i VimEventType
 lineToEvent = CL.map f where
     f line = case parseVimLine line of
         Left e                                -> InvalidEvent (show e)
